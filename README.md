6bdcb9f4994d16f4f1b9da63b50db64146370ab1
Uomonero1 <niks9498@gmail.com>
2023-05-16 12:26:45 +0200

    update README and main with new DS

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 - 03/05/23: Celsius to Fahrenheit converter with input from the console
 - 04/05/23: Algorithm that returns the number of steps for a given number to arrive to 1 (Collatz Conjecture)
-- 05/05/23: Algorithm that computes the prime below 1000
+- 05/05/23: Algorithm that computes the primes below 1000
 - 10/05/23: Simple todo app with CRUD operations, made with Gin
 - 13/05/23: Queue implementation using struct
 - 13/05/23: Stack implementation using struct
+- 16/05/23: Linked List implementation using struct
--- dataStructures/main.go
+++ dataStructures/main.go
@@ -1,27 +1,41 @@
 package main
 
+import "fmt"
+
 func main() {
-	// q := Queue{size: 2}
-	// s := Stack{size: 4}
+	/*q := Queue{size: 2}
+	s := Stack{size: 4}
 
-	// q.enqueue(1)
-	// q.enqueue(13)
-	// fmt.Println(q.values)
-	// fmt.Println(q.peek())
-	// q.dequeue()
-	// fmt.Println(q.values)
-	// q.dequeue()
-	// fmt.Println(q.values)
-	// q.dequeue()
+	q.enqueue(1)
+	q.enqueue(13)
+	fmt.Println(q.values)
+	fmt.Println(q.peek())
+	q.dequeue()
+	fmt.Println(q.values)
+	q.dequeue()
+	fmt.Println(q.values)
+	q.dequeue()
 
-	// s.peek()
-	// s.push(5)
-	// s.push(13)
-	// fmt.Println(s.values)
-	// fmt.Println("peek: ", s.peek())
-	// s.pop()
-	// fmt.Println(s.values)
-	// s.pop()
-	// fmt.Println(s.values)
-	// s.pop()
+	s.peek()
+	s.push(5)
+	s.push(13)
+	fmt.Println(s.values)
+	fmt.Println("peek: ", s.peek())
+	s.pop()
+	fmt.Println(s.values)
+	s.pop()
+	fmt.Println(s.values)
+	s.pop()*/
+
+	l := LinkedList{}
+	l.append(1)
+	fmt.Println(l.items)
+	l.append(2)
+	fmt.Println(l.items)
+	l.prepend(3)
+	fmt.Println(l.items)
+
+	for idx := range l.items {
+		fmt.Print(l.items[idx].data, "->")
+	}
 }
