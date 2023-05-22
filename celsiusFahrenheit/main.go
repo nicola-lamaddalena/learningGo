12712a983e58d4d0a730e5508ee43337e648cc11
Uomonero1 <niks9498@gmail.com>
2023-05-22 14:05:51 +0200

    added error handling

--- celsiusFahrenheit/main.go
+++ celsiusFahrenheit/main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Celsius to Fahrenheit converter")
-	fmt.Println("Insert a numeric value: ")
-
 	strInput := os.Args[1]
-	intInput, _ := strconv.Atoi(strInput)
-
+	intInput, err := strconv.Atoi(strInput)
+	if err != nil {
+		fmt.Println("You need to pass a numeric value.")
+		return
+	}
 	// Convert the Celsius value to a Fahrenheit one, print it
 	fahrenheitOutput := (float64(intInput) * 9 / 5) + 32
 	fmt.Printf("%v° Celsius degrees correspond to %v° Fahrenheit degrees.\n", intInput, fahrenheitOutput)
